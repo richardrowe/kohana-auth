af06f416201d127b1d148518791ff16098c9e2b0
richardrowe <arch.richard@gmail.com>
2009-09-06 00:57:33 +0800

    login() method in Auth_User model now passes the login() method in Auth a user object (thanks Isaiah)

--- classes/model/auth/user.php
+++ classes/model/auth/user.php
@@ -106,9 +106,9 @@ class Model_Auth_User extends ORM {
 		if ($array->check())
 		{
 			// Attempt to load the user
-			//$this->find($array['username']);
-			
-			if (Auth::instance()->login($array['username'], $array['password']))
+			$this->where('username', '=', $array['username'])->find();
+
+			if ($this->loaded() AND Auth::instance()->login($this, $array['password']))
 			{
 				if (is_string($redirect))
 				{
